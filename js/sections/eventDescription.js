5a931375b8a54f25ed4f4abea7e656647cbf143f
psynetJWlee <probe35331@gmail.com>
2025-07-10 10:54:49 +0900

    이벤트 안내 문구 수정

--- js/sections/eventDescription.js
+++ js/sections/eventDescription.js
@@ -11,11 +11,15 @@ function initEventDescriptionSection() {
       <h3 class="info-title">올킬 이벤트 안내</h3>
     </div>
     <ul class="info-list">
+      <li class="info-item">이벤트 기간 : 7월 17일 ~ 8월 17일 까지</li>
       <li class="info-item">당일 대상 경기의 승/무/패를 모두 예측하여 제출</li>
       <li class="info-item">매일 100만원을 올킬 성공자 수 N명에 따라 1/N 분배 지급</li>
       <li class="info-item">1 ~ 2개 경기 취소 시 나머지 경기 결과로 올킬 선정</li>
       <li class="info-item">3개 이상 경기 취소 시 해당일 이벤트 무효</li>
+      <li class="info-item">당일 정상적으로 종료되지 않은 경기는 '경기 취소'로 처리</li>
       <li class="info-item">당일 첫 경기 시작전까지 제출 가능</li>
+      <li class="info-item">무승부는 실제 경기 결과가 무승부일 때 한정</li>
+      <li class="info-item no-marker">프로토 승1패, 승5패와 무관</li>
     </ul>
 
     <!-- 상금 지급 안내 -->
@@ -34,6 +38,8 @@ function initEventDescriptionSection() {
       <li class="info-item">5만원 초과 시 제세공과금 (22%) 선공제 후 지급</li>
       <li class="info-item">다중 계정은 당첨자 정보 확인 후 당첨 취소 될 수 있음</li>
       <li class="info-item">당첨자 성명과 예금주명 불일치 시 당첨금 지급 불가</li>
+      <li class="info-item">이벤트 종료 후 미신청 상금은 소멸을 원칙으로 함</li>
+      <li class="info-item">단, 추후 유사 이벤트 진행 시 이월될 수 있음</li>
     </ul>
 
     <a 
